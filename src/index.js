0ab9b042f468e8d6322fe53bc8151b3be72123b1
Virenk21 <viren.d222111@gmail.com>
2022-03-16 02:14:47 +0530

    added login page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
